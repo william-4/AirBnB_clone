fee5c5b69a1aa8fb8cf2f1d5030f39becfb6d9b2
Liam <mwangiiwilliam@gmail.com>
2022-08-01 22:00:23 +0300

    task 3

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Base model
 """
+import datetime
+import uuid
 
 
 class BaseModel:
@@ -8,12 +10,31 @@ class BaseModel:
     id = 0
     created_at = 0
     update_at = 0
+    name = ""
+    my_number = None
 
     def __init__(self):
-        # self.id =
-        # self.created_at =
-        # self.updated_at =
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = self.created_at
+        self.name
 
     def __str__(self):
         """Method that returns a string representation of the class"""
-        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(
+            type(self).__name__, self.id, self.__dict__)
+
+    def save(self):
+        """ Updates updated_at with current datetime"""
+        self.update_at = datetime.datetime.now()
+
+    def to_dict(self):
+        """Returns dictionary containing all key/values of __dict__"""
+        dictionary = {}
+        dictionary["my_number"] = self.my_number
+        dictionary["name"] = self.name
+        dictionary["__class__"] = type(self).__name__
+        dictionary["updated_at"] = self.updated_at.isoformat()
+        dictionary["id"] = self.id
+        dictionary["created_at"] = self.created_at.isoformat()
+        return dictionary
