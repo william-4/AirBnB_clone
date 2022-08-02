d4d0cbdd0140f16305688ca86e030e8ea623bd5e
Tariq <tariq86hamid@gmail.com>
2022-08-02 07:40:07 +0300

    documentation tests for task 3

---
+++ tests/test_base_model.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Test module for base_model.py
+"""
+from unittest import TestCase
+import models.base_model
+
+base_class = models.base_model.BaseModel
+
+
+class TestDocumentation(TestCase):
+    """Tests for the documentation of BaseModel"""
+
+    def test_module_docs(self):
+        """Tests for the presence of the module doc string"""
+        self.assertTrue(len(models.base_model.__doc__) > 0)
+
+    def test_class_docs(self):
+        """Tests for the presence of the BaseModel doc string"""
+        self.assertTrue(len(base_class.__doc__) > 0)
+
+    def test_methods_docs(self):
+        """Tests for the presence of the BaseModel doc string"""
+        callables = [func for func in dir(base_class) if callable(getattr(base_class, func))][22:]
+        for func in callables:
+            self.assertTrue(len(func.__doc__) > 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
