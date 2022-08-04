f89c262410a04b5d56ca0b65d86dee0fb63027e4
Liam <mwangiiwilliam@gmail.com>
2022-08-04 19:41:01 +0300

    task 4 tests

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -4,12 +4,14 @@
 from unittest import TestCase
 import models.base_model
 import re
+from datetime import datetime
+
 
 base_class = models.base_model.BaseModel
 
 
-class TestDocumentation(TestCase):
-    """Tests for the documentation of BaseModel"""
+class Test_Task3(TestCase):
+    """Tests for the documentation of BaseModel and Task 3"""
 
     def test_module_docs(self):
         """Tests for the presence of the module doc string"""
@@ -36,6 +38,42 @@ class TestDocumentation(TestCase):
         base_model_instance = base_class()
         base_dict = base_model_instance.to_dict()
         self.assertTrue(datetime_pattern.match(base_dict['created_at']))
+
+class Test_Task4(TestCase):
+    """Tests for task 4"""
+
+    def test_created_at(self):
+        """Verifies the type of the attribute created_at"""
+        my_model = base_class()
+        self.assertTrue(isinstance(my_model.created_at, datetime))
+        my_model_json = my_model.to_dict()
+        self.assertTrue(isinstance(my_model_json["created_at"], str))
+        my_new_model = base_class(**my_model_json)
+        self.assertTrue(isinstance(my_new_model.created_at, datetime))
+
+    def test_updated_at(self):
+        """Verifies the type of the attribute updated_at"""
+        my_model = base_class()
+        self.assertTrue(isinstance(my_model.updated_at, datetime))
+        my_model_json = my_model.to_dict()
+        self.assertTrue(isinstance(my_model_json["updated_at"], str))
+        my_new_model = base_class(**my_model_json)
+        self.assertTrue(isinstance(my_new_model.updated_at, datetime))
+
+    def test_compare_instances(self):
+        """Verifies that my_model is not my_new_model"""
+        my_model = base_class()
+        my_model_json = my_model.to_dict()
+        my_new_model = base_class(**my_model_json)
+        self.assertFalse(my_model is my_new_model)
+
+class Test_Task5(TestCase):
+
+        
+
+    
+        
+
 
 
 if __name__ == '__main__':
